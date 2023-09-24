efad22abbb4ab78da76cad4cb731cd0e33009fa9
Patrick Zhang <zhangpatrick2004@gmail.com>
2023-09-24 03:59:09 -0400

    :3

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Model from '@/components/home/Model'
 import { Suspense, useEffect, useRef, useState } from 'react';
 import Footer from '@/components/layout/Footer';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+// import tree1 from '@/public/imgs/tree1.png'
+// import tree2 from '@/public/imgs/tree2.jpg'
 
 const inter = Manrope({ subsets: ['latin'] })
 
@@ -11,6 +14,7 @@ const climate_deadline = 18793440000;
 export default function Home() {
   const [email, setEmail] = useState("");
   const seconds = useRef<HTMLHeadingElement>(null);
+  const scrollTrigger = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (!seconds.current) return;
     const timer = setInterval(() => {
@@ -40,14 +44,14 @@ export default function Home() {
             {/* Goodb ye. */}
           </h2>
           <div className="ml-0 text-center">
-            <Link
+            <a
               className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
-              href="/"
+              href="#why"
             >
               <div className="flex text-lg">
                 <span className="justify-center">Why?</span>
               </div>
-            </Link>
+            </a>
             <Link
               className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-p5 to-p3 px-14 text-md md:mt-0 focus:shadow-outline"
               href="/buy"
@@ -78,10 +82,10 @@ export default function Home() {
         Be a Part of the Change
       </h2>
       <br></br>
-      <p className="mx-auto text-xl text-center text-gray-300 font-normal leading-relaxed fs521 lg:w-2/3">
+      <p className="mx-auto text-xl text-center max-w-full text-gray-300 font-normal leading-relaxed fs521 lg:w-2/3">
        We're spearheading the new era of carbon credits.
       </p>
-      <div className="pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3">
+      <div className="pt-12 pb-24 md:max-w-4xl max-w-full mx-auto fsac4 md:px-1 px-3">
         <div className="ktq4">
           {/* <img className="w-10" src="https://nine4.app/favicon.png"></img> */}
           <h3 className="pt-3 font-semibold text-xl text-white">
@@ -127,10 +131,10 @@ export default function Home() {
         The Stats
       </h2>
       <div className="pt-12 pb-32 max-w-screen flex flex-row flex-wrap justify-center gap-4 md:px-1 px-3">
-        <div className="ktq4 max-w-[55%] text-right whitespace-pre-line">
-          <h3 className="pt-3 font-semibold text-7xl text-white" ref={seconds}>
+        <div className="ktq4 max-w-[50em] text-right whitespace-pre-line">
+          <h3 className="pt-3 font-semibold md:text-7xl text-4xl text-white" ref={seconds}>
           </h3>
-          <h3 className="pt-3 font-semibold text-4xl text-white">
+          <h3 className="pt-3 font-semibold md:text-4xl text-2l text-white">
             seconds until the climate deadline.
           </h3>
           <p className="pt-2 value-text text-md text-gray-200 fkrr1">
@@ -139,11 +143,11 @@ export default function Home() {
             carbon neutrality as well as reduce our carbon footprint, before it's too late. 
           </p>
         </div>
-        <div className="ktq4 max-w-[35%] text-right">
-          <h3 className="pt-3 font-semibold text-7xl text-white">
+        <div className="ktq4 max-w-[30em] text-right">
+          <h3 className="pt-3 font-semibold md:text-7xl text-5xl text-white">
             19,891,392
           </h3>
-          <h3 className="pt-3 font-semibold text-4xl text-white">
+          <h3 className="pt-3 font-semibold md:text-4xl text-2xl text-white">
             unretired tokens
           </h3>
           <p className="pt-2 value-text text-md text-gray-200 fkrr1">
@@ -151,6 +155,24 @@ export default function Home() {
             atmosphere. Time will not stop for us, so we must act swiftly and decisively. 
           </p>
         </div>
+      </div>
+        <a id="why" className="relative -top-10"></a>
+      <h2 className="pt-20 mb-1 text-2xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl z-50">
+        Our Mission
+      </h2>
+      <div className="pt-12 pb-32 max-w-screen flex flex-row flex-wrap justify-center gap-4 md:px-1 px-3 relative z-10">
+        <div className="ktq4 md:max-w-[65%] max-w-[95%] text-left whitespace-pre-line z-10">
+          <p className="pt-2 value-text text-lg text-p4 fkrr1">
+            As governments push towards a sustainable future, we believe carbon credits are a solution towards reducing the net effects of climate change. As we expedite the growth of the carbon credit market, we are focusing on accessibility, transparency, and ubiquity. Individuals, businesses, and organizations can continue taking a proactive approach in pursuing carbon neutrality with us.
+          </p>
+          <div ref={scrollTrigger} className="absolute bottom-0 h-2 w-2"></div>
+        </div>
+        <motion.div className="absolute -top-20 right-[15%]" initial={{rotate: 0}} whileInView={{rotate: 40, x: 130, y: 40}}>
+          <Image src="/imgs/tree2.jpg" alt="example tree nft 2" width={200} height={200} />
+        </motion.div>
+        <motion.div className='absolute -top-20 right-[15%]' initial={{rotate: 0}} whileInView={{rotate: 20, x: 50}}>
+          <Image src="/imgs/tree.png" alt="example tree nft" width={200} height={200} />
+        </motion.div>
       </div>
       <section className="relative pb-24">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
