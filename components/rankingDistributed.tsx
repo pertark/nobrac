a80aac5a8d2846ccd1bbfb2efda5a563dd53f3c2
Patrick Zhang <zhangpatrick2004@gmail.com>
2023-09-24 07:25:46 -0400

    Nft mint

--- components/rankingDistributed.tsx
+++ components/rankingDistributed.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useEnsAvatar, useEnsName } from 'wagmi'
 
 export default function RankingDistributed(ensData,index){
@@ -26,7 +27,7 @@ console.log(ensAvatar,index)
                             : ensName.data || <span className="text-gray-400">unnamed</span>
                 }</span>
             </span>
-            <span className="text-p4">{(Number(ensData.ensData.amount) / 1000000000000000000).toFixed(2)}</span>
+            <span className="text-p4">{(ensData.index == 3) && <span className='italic underline text-blue-400 mr-4'><Link href="https://app.unlock-protocol.com/checkout?id=a314ca67-e0d0-450b-afb7-8bb70c3167ef">Mint NFT!</Link></span> }{(Number(ensData.ensData.amount) / 1000000000000000000).toFixed(2)}</span>
         </div>
     )
 }
